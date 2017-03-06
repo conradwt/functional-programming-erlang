2a6da8d553fe08c02cc76ed55988c96b1c0db2b8
Conrad Taylor <conradwt@gmail.com>
2017-03-06 15:47:38 -0800

    Add recursive implementation of perfect.

--- 1.21/tail_recursion.erl
+++ 1.21/tail_recursion.erl
@@ -1,9 +1,20 @@
 -module(tail_recursion).
--export([fib/1,fib/3]).
+-export([fib/1,fib/3, perfect/1, perfect/3]).
 
 fib(N) ->
   fib(N, 0, 1).
+
 fib(0, P, _) ->
   P;
 fib(N, P, C) ->
   fib(N - 1, C, P + C).
+
+perfect(N) ->
+  perfect(N, 1, 0).
+
+perfect(N, N, Sum) ->
+  N == Sum;
+perfect(N, M, Sum) when N rem M == 0 ->
+  perfect( N, M+1, M + Sum);
+perfect(N, M, Sum) ->
+  perfect(N, M+1, Sum).
